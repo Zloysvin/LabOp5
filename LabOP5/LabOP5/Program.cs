fe0cdfb928d8d29c0d511e641848e59c4881a69f
Zloysvin <danylomartakov@gmil.com>
2021-05-22 16:10:52 +0300

    initial commit

---
+++ LabOP5/LabOP5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabOP5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
